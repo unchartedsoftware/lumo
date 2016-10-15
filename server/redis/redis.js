8a44d68c3488b644c10be9ecb1b658a639469400
kbirk <birk.kevin@gmail.com>
2016-10-15 16:49:37 -0400

    Compress / decompress when using redis.

--- server/redis/redis.js
+++ server/redis/redis.js
@@ -3,6 +3,7 @@
     'use strict';
 
     const redis = require('redis');
+    const zlib = require('zlib');
 
     let client = null;
 
@@ -30,19 +31,27 @@
                     return;
                 }
                 if (exists) {
-                    client.get(hash, done);
+                    client.get(hash, (err, res) => {
+                        zlib.unzip(res, done);
+                    });
                 } else {
                     task((err, res) => {
                         if (err) {
                             done(err, null);
                             return;
                         }
-                        client.set(hash, res, err => {
+                        zlib.gzip(res, (err, zipped) => {
                             if (err) {
                                 done(err, null);
                                 return;
                             }
-                            done(null, res);
+                            client.set(hash, zipped, err => {
+                                if (err) {
+                                    done(err, null);
+                                    return;
+                                }
+                                done(null, res);
+                            });
                         });
                     });
                 }
