c0a298e6ffbe5331cc7668ec2759b5c7a845b917
kbirk <birk.kevin@gmail.com>
2016-10-15 12:40:54 -0400

    Update failing TilePyramid test.

--- test/TilePyramid.js
+++ test/TilePyramid.js
@@ -6,8 +6,10 @@
     const Coord = require('../src/core/Coord');
     const Layer = require('../src/layer/Layer');
     const TilePyramid = require('../src/layer/TilePyramid');
+    const Viewport = require('../src/plot/Viewport');
 
     let layer;
+    let plot;
 
     describe('TilePyramid', () => {
 
@@ -16,10 +18,21 @@
             layer.requestTile = (coord, done) => {
                 done(null, {});
             };
+            plot = {
+                zoom: 0,
+                tileSize: 256,
+                viewport: new Viewport({
+                    x: -256,
+                    y: -256,
+                    width: 512,
+                    height: 512
+                })
+            };
         });
 
         after(() => {
             layer = null;
+            plot = null;
         });
 
         describe('#constructor()', () => {
@@ -41,7 +54,7 @@
             it('should get an active tile from the pyramid', () => {
                 const pyramid = new TilePyramid(layer);
                 const coord = new Coord(0, 0, 0);
-                pyramid.requestTiles([coord]);
+                pyramid.requestTiles(plot, [ coord ]);
                 assert(pyramid.get(coord) !== undefined);
             });
             it('should return `undefined` if the pyramid does not contain the tile', () => {
